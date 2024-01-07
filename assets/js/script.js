d7e1a8bf1b9eadc4c022f7aa501c5cc205936cb3
OrxanMukhtar <a.muxtarli92@gmail.com>
2024-01-07 09:25:55 +0400

    emoji

--- assets/js/script.js
+++ assets/js/script.js
@@ -20,16 +20,26 @@ function equal() {
 
     if(inputNum === "5616183") {
 
-        inputNum = "İdris"
+        inputNum = "İdris &#10084"
+        document.getElementById('h1').innerHTML = inputNum
+    } else if(inputNum === "0505616183") {
+
+        inputNum = "&#128169"
         document.getElementById('h1').innerHTML = inputNum
     } else if(inputNum === "3009383") {
 
-        inputNum = "Cəfər"
+        inputNum = "Müdir &#10084"
         document.getElementById('h1').innerHTML = inputNum
     } else if(inputNum === "6335557") {
         inputNum = "&#128640"
         document.getElementById('h1').innerHTML = inputNum
 
+    } else if(inputNum === "553009383") {
+        inputNum = '&#10084'
+        document.getElementById('h1').innerHTML = inputNum
+    } else if(inputNum === "0553009383") {
+        inputNum = '&#128176'
+        document.getElementById('h1').innerHTML = inputNum
     }
     else {
         let equ_1 = eval(inputNum)
